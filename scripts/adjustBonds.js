deecdc62d0b2dce10982358149d3be7a87dee5b2
Otter <king@otterclam.finance>
2021-11-06 10:07:27 +0800

    chore: remove unused scripts

--- scripts/adjustBonds.js
+++ scripts/adjustBonds.js
@@ -2,14 +2,21 @@ const { ethers } = require('hardhat')
 
 async function main() {
   const signer = await ethers.getSigner()
-  const MAIBond = await ethers.getContractFactory('OtterBondDepository')
-  const maiBond = MAIBond.attach('0x28077992bFA9609Ae27458A766470b03D43dEe8A')
+  const OtterBondDepository = await ethers.getContractFactory(
+    'OtterBondDepository'
+  )
+  const bondType = 'lp'
 
-  const bcvCurrent = (await maiBond.terms())[0].toNumber()
+  const bond =
+    bondType == 'mai'
+      ? OtterBondDepository.attach('0x28077992bFA9609Ae27458A766470b03D43dEe8A')
+      : OtterBondDepository.attach('0x79B47c03B02019Af78Ee0de9B0b3Ac0786338a0d')
 
-  const add = true
-  const adjustment = 3
-  const bcvTarget = 150
+  const bcvCurrent = (await bond.terms())[0].toNumber()
+
+  const add = false
+  const adjustment = 2
+  const bcvTarget = 90
   const buffer = 0
   const step = Math.ceil(Math.abs(bcvCurrent - bcvTarget) / adjustment)
 
@@ -27,19 +34,21 @@ async function main() {
         2
       )
   )
-  await (
-    await maiBond.setAdjustment(add, adjustment, bcvTarget, buffer)
-  ).wait()
+  // await (await bond.setAdjustment(add, adjustment, bcvTarget, buffer)).wait(2)
 
-  const nonce = await signer.getTransactionCount()
-  for (let i = 0; i < step; i++) {
-    await maiBond.deposit(
-      ethers.utils.parseEther('1'),
-      ethers.utils.parseUnits('9999', 9),
-      signer.address,
-      { nonce: nonce + i }
-    )
-    console.log('adjust: ' + i)
+  console.log('adjusted')
+
+  if (bondType == 'mai') {
+    const nonce = await signer.getTransactionCount()
+    for (let i = 0; i < step; i++) {
+      await bond.deposit(
+        ethers.utils.parseEther('1'),
+        ethers.utils.parseUnits('9999', 9),
+        signer.address,
+        { nonce: nonce + i }
+      )
+      console.log('adjust: ' + i)
+    }
   }
 
   console.log('done')
