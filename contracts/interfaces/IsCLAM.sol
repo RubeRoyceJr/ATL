946dd53ee8a2ee4e0fa3d5234474605e526a4ace
joinstuff <94909719+joinstuff@users.noreply.github.com>
2021-11-24 19:43:32 +0200

    Update IsCLAM.sol

--- contracts/interfaces/IsCLAM.sol
+++ contracts/interfaces/IsCLAM.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.5;
 import "./IERC20.sol";
 
 interface IsATL is IERC20 {
-    function rebase( uint256 atlProfit_, uint epoch_) external returns (uint256);
+    function rebase( uint256 ATLProfit_, uint epoch_) external returns (uint256);
 
     function circulatingSupply() external view returns (uint256);
 
