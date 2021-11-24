80bbb6a0d2fb8f44b568483c152a8ccc8a34b0c3
joinstuff <94909719+joinstuff@users.noreply.github.com>
2021-11-24 19:36:30 +0200

    Update IOtterStaking.sol

--- contracts/interfaces/IOtterStaking.sol
+++ contracts/interfaces/IOtterStaking.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.5;
 
-interface IOtterStaking {
+interface IAtlantisStaking {
     function stake( uint _amount, address _recipient ) external returns ( bool );
     function claim ( address _recipient ) external;
 }
